def0e976eeab782e2133d7d37ac9aaa50c138ed5
rish-kun <63300310+rish-kun@users.noreply.github.com>
2026-02-21 17:03:39 +0530

    add landing page

---
+++ website/src/App.tsx
@@ -0,0 +1,13 @@
+import Hero from './components/Hero'
+import Header from './components/Header'
+
+function App() {
+  return (
+    <div className="bg-dark text-slate-50 min-h-screen">
+      <Header />
+      <Hero />
+    </div>
+  )
+}
+
+export default App
---
+++ website/vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: './',
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: '../',
+    emptyOutDir: false, // Don't empty the parent directory
+  }
+})
